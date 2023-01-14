e036a981f52c2a105aa6e96818afadd46b913045
najmdevstudio <najm.devops@gmail.com>
2023-01-14 12:11:38 +0530

    Upgrad Practice Class

---
+++ Upgrad/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int myVar;
+        myVar=50;
+        System.out.println(myVar);
+        int anotherVar = 100;
+        myVar = anotherVar;
+        System.out.println(myVar);
+        final int maxStudents = 25;
+        final int someVariable;
+        int someOtherVariable=100;
+        System.out.println(someOtherVariable);
+    }
+}
